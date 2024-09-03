3da06baea3dfc9893a958d85890757f123f2ba4a
ethan-ung <ethan.s.ung04pv@gmail.com>
2024-09-03 17:16:44 -0500

    added login/out buttons. Added carousel to right sidebar

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,6 +5,8 @@ import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 import { CarouselProps } from '@/types'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import LoaderSpinner from './LoaderSpinner'
 
 const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
     const router = useRouter();
@@ -30,31 +32,40 @@ const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
 
     const slides = fansLikeDetail && fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0)
 
+    if(!slides) return <LoaderSpinner />
+
     return (
-        <section className="embla">
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
-                    {slides.map((index) => (
-                        <div className="embla__slide" key={index}>
-                            <div className="embla__slide__number">{index + 1}</div>
-                        </div>
-                    ))}
-                </div>
+        <section className="flex w-full flex-col gap-4 overflow-hidden" ref={emblaRef}>
+            <div className='flex'>
+                {slides.slice(0, 5).map((item) => (
+                    <figure
+                        key={item._id}
+                        className='carousel_box'
+                        onClick={() => router.push(`/podcasts/${item.podcast[0]?.podcastId}`)}>
+                            <Image 
+                                src={item.imageUrl}
+                                alt='card'
+                                fill
+                                className='absolute size-full rounded-xl border-none'
+                            />
+                            <div className='glassmorphism-black relative z-10 flex flex-col rounded-b-xl p-4'>
+                                <h2 className='text-14 font-semibold text-white-1'>{item.podcast[0]?.podcastTitle}</h2>
+                                <p className='text-12 font-normal text-white-2'>{item.name}</p>
+                            </div>
+                    </figure>
+                ))}
             </div>
 
-            <div className="embla__controls">
-                <div className="embla__dots">
-                    {scrollSnaps.map((_, index) => (
-                        <DotButton
-                            key={index}
-                            onClick={() => onDotButtonClick(index)}
-                            className={'embla__dot'.concat(
-                                index === selectedIndex ? ' embla__dot--selected' : ''
-                            )}
-                        />
-                    ))}
-                </div>
-            </div>
+            
+            <div className="flex justify-center gap-2">
+                {scrollSnaps.map((_, index) => (
+                    <DotButton
+                        key={index}
+                        onClick={() => onDotButtonClick(index)}
+                        selected={index === selectedIndex}
+                    />
+                ))}
+            </div>   
         </section>
     )
 }
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -41,7 +41,7 @@ const LeftSidebar = () => {
         </SignedOut>
         <SignedIn>
             <div className='flex-center w-full pb-14 max-lg:px-4 lg:pr-8'>
-                <Button asChild className='text-16 w-full bg-orange-1 font-extrabold' onClick={() => signOut(() => router.push('/'))}>
+                <Button className='text-16 w-full bg-orange-1 font-extrabold' onClick={() => signOut(() => router.push('/'))}>
                     Log Out
                 </Button>
             </div>
